1eb73048e3ba9ba0ae60e5fe802e8dfff1d2ca78
itsukifurusawa <114970126+itsukifurusawa@users.noreply.github.com>
2022-10-17 15:34:41 +0900

    Update 2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,21 +1,11 @@
-// ギリシャ国旗
 function setup() {
-  const blue = color(0, 51, 160);
-  createCanvas(270, 180);
-  // noStroke();
-  background(255);
-
-  let d = height / 9; // 縞1本の太さ
-
-  for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
-  }
-
-  fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
-
-  fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  createCanvas(200, 200);
+  fill(255)
+  line(0,30,0,120);
+  line(0,30,200,30);
+  line(0,120,200,120);
+  line(200,30,200,120);
+  stroke(255,0,0);
+  fill(255,0,0)
+  ellipse(100,75,80)
 }
